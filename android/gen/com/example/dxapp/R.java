896ac939b4f21f616e88d855a98251a2810a3822
chui <chui101@gmail.com>
2014-03-21 15:05:02 -0400

    Started working on Drawer View for Main Activity

--- android/gen/com/example/dxapp/R.java
+++ android/gen/com/example/dxapp/R.java
@@ -24,9 +24,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int container=0x7f080000;
-        public static final int section_label=0x7f080001;
+        public static final int action_settings=0x7f080009;
+        public static final int drawer_layout=0x7f080000;
+        public static final int left_drawer=0x7f080007;
+        public static final int progressBar1=0x7f080002;
+        public static final int progressBar2=0x7f080004;
+        public static final int progressBar3=0x7f080006;
+        public static final int section_label=0x7f080008;
+        public static final int textView1=0x7f080001;
+        public static final int textView2=0x7f080003;
+        public static final int textView3=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -38,6 +45,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int searchAction=0x7f050005;
         public static final int title_section1=0x7f050002;
         public static final int title_section2=0x7f050003;
         public static final int title_section3=0x7f050004;
