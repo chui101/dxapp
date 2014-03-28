da67621e84380f97849c93a5e547b18558709ac2
Roger Chui <chui101@gmail.com>
2014-03-28 12:25:22 -0400

    Set strings for exchanges

--- android/gen/com/example/dxapp/R.java
+++ android/gen/com/example/dxapp/R.java
@@ -25,13 +25,14 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
+        public static final int action_about=0x7f08000b;
+        public static final int action_search=0x7f080009;
         public static final int action_settings=0x7f08000a;
         public static final int drawer_layout=0x7f080000;
         public static final int left_drawer=0x7f080007;
         public static final int progressBar1=0x7f080002;
         public static final int progressBar2=0x7f080004;
         public static final int progressBar3=0x7f080006;
-        public static final int search=0x7f080009;
         public static final int section_label=0x7f080008;
         public static final int textView1=0x7f080001;
         public static final int textView2=0x7f080003;
@@ -45,8 +46,12 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int aboutAction=0x7f050006;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int exchange_carb=0x7f050007;
+        public static final int exchange_fat=0x7f050009;
+        public static final int exchange_protein=0x7f050008;
         public static final int searchAction=0x7f050005;
         public static final int title_section1=0x7f050002;
         public static final int title_section2=0x7f050003;
